53c204cd5788267b32271c15b278da2c1c5a87ae
Nik <nikgois.600@gmail.com>
2025-05-21 23:16:39 +0300

    Add DiffUtil.Callback

--- app/src/main/java/com/example/directory_view/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/example/directory_view/ui/screens/home/HomeViewModel.kt
@@ -32,7 +32,6 @@ class HomeViewModel @Inject constructor(private val directoryRepository: Directo
         if (query.isEmpty()) {
             _contacts.value = allContacts
 
-
         } else {
             val searchTerms =
                 query.split(" ")
@@ -46,5 +45,4 @@ class HomeViewModel @Inject constructor(private val directoryRepository: Directo
             }
         }
     }
-
 }
